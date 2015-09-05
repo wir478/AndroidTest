bfd07bcfef1440c2a36a95409107c5789fbfd26c
wir478 <196969919@qq.com>
2015-09-06 01:41:17 +0800

    Summary嘛意思
    描述

---
+++ weixin5.2.1/src/main/java/com/gongfu/weixin521/MainActivity.java
@@ -0,0 +1,23 @@
+package com.gongfu.weixin521;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+
+    }
+
+}
